20d332f32b3118c3d8f971ec98d3412d83986dfa
LutfiNR <lutfinurrohman5@gmail.com>
2023-02-27 23:32:37 +0700

    Tugas Final

--- lutfi_mealdb/app/src/main/java/com/bootcamp/lutfi_mealdb/ui/DetailActivity.kt
+++ lutfi_mealdb/app/src/main/java/com/bootcamp/lutfi_mealdb/ui/DetailActivity.kt
@@ -1,6 +1,8 @@
 package com.bootcamp.lutfi_mealdb.ui
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
